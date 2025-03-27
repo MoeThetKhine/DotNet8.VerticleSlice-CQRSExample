89ca067cf90d2dc08c8ffcf66be25bf1f897ce1c
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-28 01:08:17 +0630

    -

--- DotNet8.VerticleSlice&CQRSExample.Models/Setup/Blog/BlogModel.cs
+++ DotNet8.VerticleSlice&CQRSExample.Models/Setup/Blog/BlogModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.VerticleSlice_CQRSExample.Models.Setup.Blog
+﻿namespace DotNet8.VerticleSlice_CQRSExample.Models.Setup.Blog
 {
 	public class BlogModel
 	{
