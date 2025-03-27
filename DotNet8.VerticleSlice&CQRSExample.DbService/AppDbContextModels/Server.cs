49031daf43cf4912c1425eff317e8ae869b86dec
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-27 20:45:19 +0630

    -

---
+++ DotNet8.VerticleSlice&CQRSExample.DbService/AppDbContextModels/Server.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet8.VerticleSlice_CQRSExample.DbService.AppDbContextModels;
+
+public partial class Server
+{
+    public string Id { get; set; } = null!;
+
+    public string? Data { get; set; }
+
+    public DateTime LastHeartbeat { get; set; }
+}
