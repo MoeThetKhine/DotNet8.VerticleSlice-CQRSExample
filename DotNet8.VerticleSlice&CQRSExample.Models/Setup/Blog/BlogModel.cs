e3b08aa8d701599a8d4e9186cce564ff677f97b6
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-28 01:08:06 +0630

    Feat : Add Blog Model

--- DotNet8.VerticleSlice&CQRSExample.Models/Setup/Blog/BlogModel.cs
+++ DotNet8.VerticleSlice&CQRSExample.Models/Setup/Blog/BlogModel.cs
@@ -8,5 +8,12 @@ namespace DotNet8.VerticleSlice_CQRSExample.Models.Setup.Blog
 {
 	public class BlogModel
 	{
+		public long BlogId { get; set; }
+
+		public string BlogTitle { get; set; } = null!;
+
+		public string BlogAuthor { get; set; } = null!;
+
+		public string BlogContent { get; set; } = null!;
 	}
 }
