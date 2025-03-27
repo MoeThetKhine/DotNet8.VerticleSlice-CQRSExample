c76f3716c2a95a7c04dbc10287ba9dc223956e59
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-28 01:10:16 +0630

    Format Code

--- DotNet8.VerticleSlice&CQRSExample.Models/Setup/Blog/BlogRequestModel.cs
+++ DotNet8.VerticleSlice&CQRSExample.Models/Setup/Blog/BlogRequestModel.cs
@@ -1,11 +1,10 @@
-﻿namespace DotNet8.VerticleSlice_CQRSExample.Models.Setup.Blog
+﻿namespace DotNet8.VerticleSlice_CQRSExample.Models.Setup.Blog;
+
+public class BlogRequestModel
 {
-	public class BlogRequestModel
-	{
-		public string BlogTitle { get; set; } = null!;
+	public string BlogTitle { get; set; } = null!;
 
-		public string BlogAuthor { get; set; } = null!;
+	public string BlogAuthor { get; set; } = null!;
 
-		public string BlogContent { get; set; } = null!;
-	}
+	public string BlogContent { get; set; } = null!;
 }
