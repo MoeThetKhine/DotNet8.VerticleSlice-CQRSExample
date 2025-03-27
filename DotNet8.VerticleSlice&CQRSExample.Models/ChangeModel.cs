d870d63326d7afc162cf43acf6792e1b190a57ac
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-28 01:18:05 +0630

    Format Code

--- DotNet8.VerticleSlice&CQRSExample.Models/ChangeModel.cs
+++ DotNet8.VerticleSlice&CQRSExample.Models/ChangeModel.cs
@@ -1,29 +1,28 @@
 ﻿using DotNet8.VerticleSlice_CQRSExample.DbService.AppDbContextModels;
 using DotNet8.VerticleSlice_CQRSExample.Models.Setup.Blog;
 
-namespace DotNet8.VerticleSlice_CQRSExample.Models
-{
-	public static class ChangeModel
-	{
-		public static BlogModel Change(this TblBlog dataModel)
-		{
-			return new BlogModel
-			{
-				BlogId = dataModel.BlogId,
-				BlogTitle = dataModel.BlogTitle,
-				BlogAuthor = dataModel.BlogAuthor,
-				BlogContent = dataModel.BlogContent,
-			};
-		}
+namespace DotNet8.VerticleSlice_CQRSExample.Models;
 
-		public static TblBlog Change(this BlogRequestModel requestModel)
+public static class ChangeModel
+{
+	public static BlogModel Change(this TblBlog dataModel)
+	{
+		return new BlogModel
 		{
-			return new TblBlog
-			{
-				BlogTitle = requestModel.BlogTitle,
-				BlogAuthor = requestModel.BlogAuthor,
-				BlogContent = requestModel.BlogContent,
-			};
-		}
+			BlogId = dataModel.BlogId,
+			BlogTitle = dataModel.BlogTitle,
+			BlogAuthor = dataModel.BlogAuthor,
+			BlogContent = dataModel.BlogContent,
+		};
+	}
+
+	public static TblBlog Change(this BlogRequestModel requestModel)
+	{
+		return new TblBlog
+		{
+			BlogTitle = requestModel.BlogTitle,
+			BlogAuthor = requestModel.BlogAuthor,
+			BlogContent = requestModel.BlogContent,
+		};
 	}
 }
