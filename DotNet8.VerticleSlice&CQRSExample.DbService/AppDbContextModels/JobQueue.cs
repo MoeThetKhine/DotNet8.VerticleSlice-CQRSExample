4803ca38b415c455ddd0781a3809faa458ed78e5
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-27 20:44:46 +0630

    -

---
+++ DotNet8.VerticleSlice&CQRSExample.DbService/AppDbContextModels/JobQueue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet8.VerticleSlice_CQRSExample.DbService.AppDbContextModels;
+
+public partial class JobQueue
+{
+    public long Id { get; set; }
+
+    public long JobId { get; set; }
+
+    public string Queue { get; set; } = null!;
+
+    public DateTime? FetchedAt { get; set; }
+}
