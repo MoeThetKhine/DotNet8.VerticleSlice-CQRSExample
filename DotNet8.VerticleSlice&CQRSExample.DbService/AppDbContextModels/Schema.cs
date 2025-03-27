a2633f8ae7ad745fb5622fae4d5234bb0095f02b
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-27 20:52:36 +0630

    -

--- DotNet8.VerticleSlice&CQRSExample.DbService/AppDbContextModels/Schema.cs
+++ DotNet8.VerticleSlice&CQRSExample.DbService/AppDbContextModels/Schema.cs
@@ -1,6 +1,10 @@
 ﻿namespace DotNet8.VerticleSlice_CQRSExample.DbService.AppDbContextModels;
 
+#region Schema
+
 public partial class Schema
 {
     public int Version { get; set; }
 }
+
+#endregion
