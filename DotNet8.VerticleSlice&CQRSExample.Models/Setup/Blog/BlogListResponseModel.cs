10d7bc9ed42c34e5c72fbf4172acd034ebf686de
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-28 01:12:48 +0630

    -

--- DotNet8.VerticleSlice&CQRSExample.Models/Setup/Blog/BlogListResponseModel.cs
+++ DotNet8.VerticleSlice&CQRSExample.Models/Setup/Blog/BlogListResponseModel.cs
@@ -1,6 +1,10 @@
 ﻿namespace DotNet8.VerticleSlice_CQRSExample.Models.Setup.Blog;
 
+#region BlogListResponseModel
+
 public class BlogListResponseModel
 {
 	public List<BlogModel> DataLst { get; set; }
 }
+
+#endregion
