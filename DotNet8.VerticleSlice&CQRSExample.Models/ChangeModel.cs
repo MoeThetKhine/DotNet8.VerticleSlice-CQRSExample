8a8463acf3f54948cfc81ca9c8b8e3ef10cc1bb1
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-28 01:17:27 +0630

    Feat : Add ChangeModel

--- DotNet8.VerticleSlice&CQRSExample.Models/ChangeModel.cs
+++ DotNet8.VerticleSlice&CQRSExample.Models/ChangeModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DotNet8.VerticleSlice_CQRSExample.DbService.AppDbContextModels;
+using DotNet8.VerticleSlice_CQRSExample.Models.Setup.Blog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,27 @@ using System.Threading.Tasks;
 
 namespace DotNet8.VerticleSlice_CQRSExample.Models
 {
-	public class ChangeModel
+	public static class ChangeModel
 	{
+		public static BlogModel Change(this TblBlog dataModel)
+		{
+			return new BlogModel
+			{
+				BlogId = dataModel.BlogId,
+				BlogTitle = dataModel.BlogTitle,
+				BlogAuthor = dataModel.BlogAuthor,
+				BlogContent = dataModel.BlogContent,
+			};
+		}
+
+		public static TblBlog Change(this BlogRequestModel requestModel)
+		{
+			return new TblBlog
+			{
+				BlogTitle = requestModel.BlogTitle,
+				BlogAuthor = requestModel.BlogAuthor,
+				BlogContent = requestModel.BlogContent,
+			};
+		}
 	}
 }
