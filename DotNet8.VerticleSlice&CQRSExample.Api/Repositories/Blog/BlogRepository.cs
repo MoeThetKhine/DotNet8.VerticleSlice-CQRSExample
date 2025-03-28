65f5d475abe7de954ace7c94e52c1e494eb4ef7a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-28 11:38:38 +0630

    Format Code

--- DotNet8.VerticleSlice&CQRSExample.Api/Repositories/Blog/BlogRepository.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/Repositories/Blog/BlogRepository.cs
@@ -2,39 +2,38 @@
 using DotNet8.VerticleSlice_CQRSExample.Models;
 using DotNet8.VerticleSlice_CQRSExample.Models.Setup.Blog;
 
-namespace DotNet8.VerticleSlice_CQRSExample.Api.Repositories.Blog
+namespace DotNet8.VerticleSlice_CQRSExample.Api.Repositories.Blog;
+
+public class BlogRepository : IBlogRepository
 {
-	public class BlogRepository : IBlogRepository
+	private readonly AppDbContext _appDbContext;
+
+	public BlogRepository(AppDbContext appDbContext)
 	{
-		private readonly AppDbContext _appDbContext;
+		_appDbContext = appDbContext;
+	}
 
-		public BlogRepository(AppDbContext appDbContext)
+	public async Task<BlogListResponseModel> GetBlogListAsync()
+	{
+		try
 		{
-			_appDbContext = appDbContext;
+			var dataLst = await _appDbContext.TblBlogs
+				.AsNoTracking()
+				.OrderByDescending(x => x.BlogId)
+				.ToListAsync();
+
+			var lst = dataLst.Select(x => x.Change()).ToList();
+
+			BlogListResponseModel responseModel = new()
+			{
+				DataLst = lst
+			};
+
+			return responseModel;
 		}
-
-		public async Task<BlogListResponseModel> GetBlogListAsync()
+		catch (Exception ex)
 		{
-			try
-			{
-				var dataLst = await _appDbContext.TblBlogs
-					.AsNoTracking()
-					.OrderByDescending(x => x.BlogId)
-					.ToListAsync();
-
-				var lst = dataLst.Select(x => x.Change()).ToList();
-
-				BlogListResponseModel responseModel = new()
-				{
-					DataLst = lst
-				};
-
-				return responseModel;
-			}
-			catch (Exception ex)
-			{
-				throw new Exception(ex.Message);
-			}
+			throw new Exception(ex.Message);
 		}
 	}
 }
