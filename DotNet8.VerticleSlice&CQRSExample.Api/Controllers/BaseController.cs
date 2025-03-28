f2a8352578df2d829375675ce69c29146dc221ed
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-28 10:55:23 +0630

    Format Code

--- DotNet8.VerticleSlice&CQRSExample.Api/Controllers/BaseController.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/Controllers/BaseController.cs
@@ -2,46 +2,45 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
-namespace DotNet8.VerticleSlice_CQRSExample.Api.Controllers
+namespace DotNet8.VerticleSlice_CQRSExample.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class BaseController : ControllerBase
+	#region Content
+
+	protected IActionResult Content(object? obj)
 	{
-		#region Content
-
-		protected IActionResult Content(object? obj)
-		{
-			return Content(JsonConvert.SerializeObject(obj), "application/json");
-		}
-
-		#endregion
-
-		#region InternalServerError
-
-		protected IActionResult InternalServerError(Exception ex)
-		{
-			return StatusCode(500, ex.Message);
-		}
-
-		#endregion
-
-		#region Created
-
-		protected IActionResult Created(string message)
-		{
-			return StatusCode(201, message);
-		}
-
-		#endregion
-
-		#region Accepted
-
-		protected IActionResult Accepted(string message)
-		{
-			return StatusCode(202, message);
-		}
-
-		#endregion
+		return Content(JsonConvert.SerializeObject(obj), "application/json");
 	}
+
+	#endregion
+
+	#region InternalServerError
+
+	protected IActionResult InternalServerError(Exception ex)
+	{
+		return StatusCode(500, ex.Message);
+	}
+
+	#endregion
+
+	#region Created
+
+	protected IActionResult Created(string message)
+	{
+		return StatusCode(201, message);
+	}
+
+	#endregion
+
+	#region Accepted
+
+	protected IActionResult Accepted(string message)
+	{
+		return StatusCode(202, message);
+	}
+
+	#endregion
 }
