177db7f990b60ae3e2f1da4d0a1ef4494e5f03d0
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-28 11:08:47 +0630

    Feat : Add AddRepositoryServices

--- DotNet8.VerticleSlice&CQRSExample.Api/ModularService.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/ModularService.cs
@@ -1,6 +1,15 @@
-﻿namespace DotNet8.VerticleSlice_CQRSExample.Api
+﻿using DotNet8.VerticleSlice_CQRSExample.Api.Repositories.Blog;
+
+namespace DotNet8.VerticleSlice_CQRSExample.Api
 {
-	public class ModularService
+	public static class ModularService
 	{
+
+		private static IServiceCollection AddRepositoryServices(this IServiceCollection services)
+		{
+			services.AddScoped<IBlogRepository, BlogRepository>();
+			return services;
+		}
+
 	}
 }
