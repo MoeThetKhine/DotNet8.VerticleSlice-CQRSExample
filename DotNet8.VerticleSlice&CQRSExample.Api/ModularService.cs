0f2f02606d3005e4c214cc2aaa2cb80c525d0e29
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-28 11:24:39 +0630

    Format Code

--- DotNet8.VerticleSlice&CQRSExample.Api/ModularService.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/ModularService.cs
@@ -2,52 +2,51 @@
 using DotNet8.VerticleSlice_CQRSExample.DbService.AppDbContextModels;
 using Microsoft.EntityFrameworkCore;
 
-namespace DotNet8.VerticleSlice_CQRSExample.Api
+namespace DotNet8.VerticleSlice_CQRSExample.Api;
+
+public static class ModularService
 {
-	public static class ModularService
+	public static IServiceCollection AddServices(this IServiceCollection services, WebApplicationBuilder builder)
 	{
-		public static IServiceCollection AddServices(this IServiceCollection services, WebApplicationBuilder builder)
-		{
-			services.AddRepositoryServices()
-				.AddDbContextService(builder)
-				.AddMediatRService()
-				.AddJsonServices();
+		services.AddRepositoryServices()
+			.AddDbContextService(builder)
+			.AddMediatRService()
+			.AddJsonServices();
 
-			return services;
-		}
-
-		private static IServiceCollection AddRepositoryServices(this IServiceCollection services)
-		{
-			services.AddScoped<IBlogRepository, BlogRepository>();
-			return services;
-		}
-
-		private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
-		{
-			builder.Services.AddDbContext<AppDbContext>(opt =>
-			{
-				opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-			}, ServiceLifetime.Transient);
-
-			return services;
-		}
-
-		private static IServiceCollection AddMediatRService(this IServiceCollection services)
-		{
-			services.AddMediatR(cf => cf.RegisterServicesFromAssembly(typeof(Program).Assembly));
-			return services;
-		}
-
-		private static IServiceCollection AddJsonServices(this IServiceCollection services)
-		{
-			services.AddControllers().AddJsonOptions(opt =>
-			{
-				opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-			});
-
-			return services;
-		}
-
-
+		return services;
 	}
+
+	private static IServiceCollection AddRepositoryServices(this IServiceCollection services)
+	{
+		services.AddScoped<IBlogRepository, BlogRepository>();
+		return services;
+	}
+
+	private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+	{
+		builder.Services.AddDbContext<AppDbContext>(opt =>
+		{
+			opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+		}, ServiceLifetime.Transient);
+
+		return services;
+	}
+
+	private static IServiceCollection AddMediatRService(this IServiceCollection services)
+	{
+		services.AddMediatR(cf => cf.RegisterServicesFromAssembly(typeof(Program).Assembly));
+		return services;
+	}
+
+	private static IServiceCollection AddJsonServices(this IServiceCollection services)
+	{
+		services.AddControllers().AddJsonOptions(opt =>
+		{
+			opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+		});
+
+		return services;
+	}
+
+
 }
