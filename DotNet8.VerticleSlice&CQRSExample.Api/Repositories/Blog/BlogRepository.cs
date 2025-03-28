24a29ca5fe954c04124e19d3053c0aafe6d4880d
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-28 11:12:55 +0630

    -

--- DotNet8.VerticleSlice&CQRSExample.Api/Repositories/Blog/BlogRepository.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/Repositories/Blog/BlogRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace DotNet8.VerticleSlice_CQRSExample.Api.Repositories.Blog
+﻿using DotNet8.VerticleSlice_CQRSExample.DbService.AppDbContextModels;
+
+namespace DotNet8.VerticleSlice_CQRSExample.Api.Repositories.Blog
 {
 	public class BlogRepository : IBlogRepository
 	{
+		private readonly AppDbContext _appDbContext;
+
+		public BlogRepository(AppDbContext appDbContext)
+		{
+			_appDbContext = appDbContext;
+		}
 	}
 }
