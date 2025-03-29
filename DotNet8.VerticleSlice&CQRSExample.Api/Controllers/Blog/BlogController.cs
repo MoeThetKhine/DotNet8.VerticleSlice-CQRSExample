0aaceb9ca2939e9141b447b77a2a10942aa83365
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-29 15:05:43 +0630

    Feat : Add GetBlogs in Controller

--- DotNet8.VerticleSlice&CQRSExample.Api/Controllers/Blog/BlogController.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/Controllers/Blog/BlogController.cs
@@ -1,11 +1,34 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DotNet8.VerticleSlice_CQRSExample.Api.Features.Blog.Queries.GetBlogList;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNet8.VerticleSlice_CQRSExample.Api.Controllers.Blog
 {
 	[Route("api/[controller]")]
 	[ApiController]
-	public class BlogController : ControllerBase
+	public class BlogController : BaseController
 	{
+		private readonly IMediator _mediator;
+
+		public BlogController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetBlogs()
+		{
+			try
+			{
+				var query = new GetBlogListQuery();
+				var lst = await _mediator.Send(query);
+
+				return Content(lst);
+			}
+			catch (Exception ex)
+			{
+				return InternalServerError(ex);
+			}
+		}
 	}
 }
