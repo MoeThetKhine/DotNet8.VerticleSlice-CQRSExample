0abc46334947bde9ff777f71de36fa81ba7dcfda
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-29 12:04:56 +0630

    -

--- DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Queries/GetBlogList/GetBlogListQuery.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Queries/GetBlogList/GetBlogListQuery.cs
@@ -2,6 +2,10 @@
 
 namespace DotNet8.VerticleSlice_CQRSExample.Api.Features.Blog.Queries.GetBlogList;
 
+#region GetBlogListQuery
+
 public class GetBlogListQuery : IRequest<BlogListResponseModel>
 {
 }
+
+#endregion
