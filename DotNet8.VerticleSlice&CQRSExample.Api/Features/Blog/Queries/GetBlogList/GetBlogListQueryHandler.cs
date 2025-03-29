c27feaaef7acb29f9b28027e2329f3089ae48740
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-29 12:11:53 +0630

    Feat : Add GetBlogListQueryHandler

--- DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Queries/GetBlogList/GetBlogListQueryHandler.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Queries/GetBlogList/GetBlogListQueryHandler.cs
@@ -1,6 +1,17 @@
 ﻿namespace DotNet8.VerticleSlice_CQRSExample.Api.Features.Blog.Queries.GetBlogList
 {
-	public class GetBlogListQueryHandler
+	public class GetBlogListQueryHandler : IRequestHandler<GetBlogListQuery, BlogListResponseModel>
 	{
+		private readonly IBlogRepository _blogRepository;
+
+		public GetBlogListQueryHandler(IBlogRepository blogRepository)
+		{
+			_blogRepository = blogRepository;
+		}
+
+		public async Task<BlogListResponseModel> Handle(GetBlogListQuery request, CancellationToken cancellationToken)
+		{
+			return await _blogRepository.GetBlogsAsync();
+		}
 	}
 }
