44df7d7212406a181113c8e6bb09954e30b570ce
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-29 12:12:09 +0630

    -

--- DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Queries/GetBlogList/GetBlogListQueryHandler.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Queries/GetBlogList/GetBlogListQueryHandler.cs
@@ -1,17 +1,16 @@
-﻿namespace DotNet8.VerticleSlice_CQRSExample.Api.Features.Blog.Queries.GetBlogList
+﻿namespace DotNet8.VerticleSlice_CQRSExample.Api.Features.Blog.Queries.GetBlogList;
+
+public class GetBlogListQueryHandler : IRequestHandler<GetBlogListQuery, BlogListResponseModel>
 {
-	public class GetBlogListQueryHandler : IRequestHandler<GetBlogListQuery, BlogListResponseModel>
+	private readonly IBlogRepository _blogRepository;
+
+	public GetBlogListQueryHandler(IBlogRepository blogRepository)
 	{
-		private readonly IBlogRepository _blogRepository;
+		_blogRepository = blogRepository;
+	}
 
-		public GetBlogListQueryHandler(IBlogRepository blogRepository)
-		{
-			_blogRepository = blogRepository;
-		}
-
-		public async Task<BlogListResponseModel> Handle(GetBlogListQuery request, CancellationToken cancellationToken)
-		{
-			return await _blogRepository.GetBlogsAsync();
-		}
+	public async Task<BlogListResponseModel> Handle(GetBlogListQuery request, CancellationToken cancellationToken)
+	{
+		return await _blogRepository.GetBlogsAsync();
 	}
 }
