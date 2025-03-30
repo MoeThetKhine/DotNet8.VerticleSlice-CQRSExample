ef3ec8dc7ef958cc080d0017c3e8b62a0834d34c
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-30 14:59:47 +0630

    Format Code

--- DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Command/DeleteBlog/DeleteBlogCommand.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Command/DeleteBlog/DeleteBlogCommand.cs
@@ -1,7 +1,6 @@
-﻿namespace DotNet8.VerticleSlice_CQRSExample.Api.Features.Blog.Command.DeleteBlog
+﻿namespace DotNet8.VerticleSlice_CQRSExample.Api.Features.Blog.Command.DeleteBlog;
+
+public class DeleteBlogCommand : IRequest<int>
 {
-	public class DeleteBlogCommand : IRequest<int>
-	{
-		public long BlogId {  get; set; }
-	}
+	public long BlogId {  get; set; }
 }
