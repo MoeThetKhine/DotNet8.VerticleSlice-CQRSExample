21512e7789b3609cd560ca9f7a3f0d06a9c49634
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-30 14:59:30 +0630

    Feat : Add DeleteBlogCommand

--- DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Command/DeleteBlog/DeleteBlogCommand.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Command/DeleteBlog/DeleteBlogCommand.cs
@@ -1,6 +1,7 @@
 ﻿namespace DotNet8.VerticleSlice_CQRSExample.Api.Features.Blog.Command.DeleteBlog
 {
-	public class DeleteBlogCommand
+	public class DeleteBlogCommand : IRequest<int>
 	{
+		public long BlogId {  get; set; }
 	}
 }
