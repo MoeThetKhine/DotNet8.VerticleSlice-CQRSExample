6dac5b32d2f691c167b3b04dc7a20f57c4abd743
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-30 12:27:01 +0630

    Feat : Add CreateBlogCommandHandler

--- DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Command/CreateBlog/CreateBlogCommandHandler.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Command/CreateBlog/CreateBlogCommandHandler.cs
@@ -1,6 +1,26 @@
 ﻿namespace DotNet8.VerticleSlice_CQRSExample.Api.Features.Blog.Command.CreateBlog
 {
-	public class CreateBlogCommandHandler
+	public class CreateBlogCommandHandler : IRequestHandler<CreateBlogCommand , int>
 	{
+		private readonly IBlogRepository _blogRepository;
+
+		public CreateBlogCommandHandler(IBlogRepository blogRepository)
+		{
+			_blogRepository = blogRepository;
+		}
+
+		public async Task<int> Handle(CreateBlogCommand request, CancellationToken cancellationToken)
+		{
+			if (string.IsNullOrEmpty(request.BlogRequestModel.BlogTitle))
+				throw new Exception("Blog Title cannot be empty.");
+
+			if (string.IsNullOrEmpty(request.BlogRequestModel.BlogAuthor))
+				throw new Exception("Blog Author cannot be empty.");
+
+			if (string.IsNullOrEmpty(request.BlogRequestModel.BlogContent))
+				throw new Exception("Blog Content cannot be empty.");
+
+			return await _blogRepository.CreateBlogAsync(request.BlogRequestModel);
+		}
 	}
 }
