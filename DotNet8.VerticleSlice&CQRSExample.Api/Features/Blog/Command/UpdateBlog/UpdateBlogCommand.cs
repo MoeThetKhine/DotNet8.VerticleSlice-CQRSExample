76b83d97351f8b4bf910ee50f2bba4584ccec8f3
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-30 14:20:46 +0630

    Feat : Add UpdateBlogCommand

--- DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Command/UpdateBlog/UpdateBlogCommand.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Command/UpdateBlog/UpdateBlogCommand.cs
@@ -1,6 +1,8 @@
 ﻿namespace DotNet8.VerticleSlice_CQRSExample.Api.Features.Blog.Command.UpdateBlog
 {
-	public class UpdateBlogCommand
+	public class UpdateBlogCommand  : IRequest<int>
 	{
+		public BlogRequestModel BlogRequestModel { get; set; }
+		public long BlogId {  get; set; }
 	}
 }
--- DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Command/UpdateBlog/UpdateBlogCommandHandler.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Command/UpdateBlog/UpdateBlogCommandHandler.cs
@@ -1,6 +1,19 @@
 ﻿namespace DotNet8.VerticleSlice_CQRSExample.Api.Features.Blog.Command.UpdateBlog
 {
-	public class UpdateBlogCommandHandler
+	public class UpdateBlogCommandHandler : IRequestHandler<UpdateBlogCommand, int>
 	{
+		private readonly IBlogRepository _blogRepository;
+
+		public UpdateBlogCommandHandler(IBlogRepository blogRepository)
+		{
+			_blogRepository = blogRepository;
+		}
+
+		public async Task<int> Handle(UpdateBlogCommand request, CancellationToken cancellationToken)
+		{
+			if (request.BlogId <= 0)
+				throw new Exception("Blog Id cannot be empty.");
+
+		}
 	}
 }
