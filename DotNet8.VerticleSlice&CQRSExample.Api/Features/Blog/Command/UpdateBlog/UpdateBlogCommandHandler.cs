04cbcc49517445b65ab7b44cb635506b6894d48e
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-30 14:35:19 +0630

    Format Code

--- DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Command/UpdateBlog/UpdateBlogCommandHandler.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Command/UpdateBlog/UpdateBlogCommandHandler.cs
@@ -1,20 +1,19 @@
-﻿namespace DotNet8.VerticleSlice_CQRSExample.Api.Features.Blog.Command.UpdateBlog
+﻿namespace DotNet8.VerticleSlice_CQRSExample.Api.Features.Blog.Command.UpdateBlog;
+
+public class UpdateBlogCommandHandler : IRequestHandler<UpdateBlogCommand, int>
 {
-	public class UpdateBlogCommandHandler : IRequestHandler<UpdateBlogCommand, int>
+	private readonly IBlogRepository _blogRepository;
+
+	public UpdateBlogCommandHandler(IBlogRepository blogRepository)
 	{
-		private readonly IBlogRepository _blogRepository;
+		_blogRepository = blogRepository;
+	}
 
-		public UpdateBlogCommandHandler(IBlogRepository blogRepository)
-		{
-			_blogRepository = blogRepository;
-		}
+	public async Task<int> Handle(UpdateBlogCommand request, CancellationToken cancellationToken)
+	{
+		if (request.BlogId <= 0)
+			throw new Exception("Blog Id cannot be empty.");
 
-		public async Task<int> Handle(UpdateBlogCommand request, CancellationToken cancellationToken)
-		{
-			if (request.BlogId <= 0)
-				throw new Exception("Blog Id cannot be empty.");
-
-			return await _blogRepository.UpdateBlogAsync(request.BlogRequestModel, request.BlogId);
-		}
+		return await _blogRepository.UpdateBlogAsync(request.BlogRequestModel, request.BlogId);
 	}
 }
