8f29f0f4286351ea3351da643ee53405210403c6
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-30 12:23:55 +0630

    -

--- DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Command/CreateBlog/CreateBlogCommand.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Command/CreateBlog/CreateBlogCommand.cs
@@ -1,6 +1,10 @@
 ﻿namespace DotNet8.VerticleSlice_CQRSExample.Api.Features.Blog.Command.CreateBlog;
 
+#region CreateBlogCommand
+
 public class CreateBlogCommand : IRequest<int>
 {
 	public BlogRequestModel BlogRequestModel { get; set; }
 }
+
+#endregion
