e62adb83d13480fc96e1cdf2d04e485893c6fd88
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-30 11:20:41 +0630

    Feat : Add GetBlogByIdQueryHandler

--- DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Queries/GetBlogById/GetBlogByIdQueryHandler.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Queries/GetBlogById/GetBlogByIdQueryHandler.cs
@@ -1,6 +1,20 @@
 ﻿namespace DotNet8.VerticleSlice_CQRSExample.Api.Features.Blog.Queries.GetBlogById
 {
-	public class GetBlogByIdQueryHandler
+	public class GetBlogByIdQueryHandler : IRequestHandler<GetBlogByIdQuery, BlogModel>
 	{
+		private readonly IBlogRepository _blogRepository;
+
+		public GetBlogByIdQueryHandler(IBlogRepository blogRepository)
+		{
+			_blogRepository = blogRepository;
+		}
+
+		public async Task<BlogModel> Handle(GetBlogByIdQuery request, CancellationToken cancellationToken)
+		{
+			if (request.BlogId <= 0)
+				throw new Exception("Id cannot be empty");
+
+			return await _blogRepository.GetBlogByIdAsync(request.BlogId);
+		}
 	}
 }
