a7a8a532d296d7ebaae22b2551fa09ea29903bd9
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-30 11:19:49 +0630

    Feat : Add GetBlogByIdAsync in IBlogRepository

--- DotNet8.VerticleSlice&CQRSExample.Api/Repositories/Blog/IBlogRepository.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/Repositories/Blog/IBlogRepository.cs
@@ -5,4 +5,5 @@ namespace DotNet8.VerticleSlice_CQRSExample.Api.Repositories.Blog;
 public interface IBlogRepository
 {
 	Task<BlogListResponseModel> GetBlogsAsync();
+	Task<BlogModel> GetBlogByIdAsync(long id);
 }
