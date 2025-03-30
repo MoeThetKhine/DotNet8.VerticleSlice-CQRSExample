a8ae4c3770d0220a6620c3b3d55e57382701472d
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-30 11:25:15 +0630

    -

--- DotNet8.VerticleSlice&CQRSExample.Api/Repositories/Blog/IBlogRepository.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/Repositories/Blog/IBlogRepository.cs
@@ -1,7 +1,11 @@
 ﻿namespace DotNet8.VerticleSlice_CQRSExample.Api.Repositories.Blog;
 
+#region IBlogRepository
+
 public interface IBlogRepository
 {
 	Task<BlogListResponseModel> GetBlogsAsync();
 	Task<BlogModel> GetBlogByIdAsync(long id);
 }
+
+#endregion
