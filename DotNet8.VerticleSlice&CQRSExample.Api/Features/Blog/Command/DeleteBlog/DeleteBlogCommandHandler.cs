1f907da690ee9866d75a29593aeb333df0820709
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-30 15:10:07 +0630

    Feat : Add DeleteBlogCommandHandler

--- DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Command/DeleteBlog/DeleteBlogCommandHandler.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Command/DeleteBlog/DeleteBlogCommandHandler.cs
@@ -1,6 +1,20 @@
 ﻿namespace DotNet8.VerticleSlice_CQRSExample.Api.Features.Blog.Command.DeleteBlog
 {
-	public class DeleteBlogCommandHandler
+	public class DeleteBlogCommandHandler : IRequestHandler<DeleteBlogCommand, int>
 	{
+		private readonly IBlogRepository _blogRepository;
+
+		public DeleteBlogCommandHandler(IBlogRepository blogRepository)
+		{
+			_blogRepository = blogRepository;
+		}
+
+		public async Task<int> Handle(DeleteBlogCommand request, CancellationToken cancellationToken)
+		{
+			if (request.BlogId <= 0)
+				throw new Exception("Id is invalid.");
+
+			return await _blogRepository.DeleteBlogAsync(request.BlogId);
+		}
 	}
 }
