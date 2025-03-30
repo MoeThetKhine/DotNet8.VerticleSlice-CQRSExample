dc25d1e2c97c1724d8262d9d1b6c90296201901a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-30 11:40:35 +0630

    Format Code

--- DotNet8.VerticleSlice&CQRSExample.Api/Controllers/Blog/BlogController.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/Controllers/Blog/BlogController.cs
@@ -1,58 +1,57 @@
 ﻿using DotNet8.VerticleSlice_CQRSExample.Api.Features.Blog.Queries.GetBlogById;
 using DotNet8.VerticleSlice_CQRSExample.Api.Features.Blog.Queries.GetBlogList;
 
-namespace DotNet8.VerticleSlice_CQRSExample.Api.Controllers.Blog
+namespace DotNet8.VerticleSlice_CQRSExample.Api.Controllers.Blog;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : BaseController
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class BlogController : BaseController
+	private readonly IMediator _mediator;
+
+	public BlogController(IMediator mediator)
 	{
-		private readonly IMediator _mediator;
-
-		public BlogController(IMediator mediator)
-		{
-			_mediator = mediator;
-		}
-
-		#region GetBlogs
-
-		[HttpGet]
-		public async Task<IActionResult> GetBlogs()
-		{
-			try
-			{
-				var query = new GetBlogListQuery();
-				var lst = await _mediator.Send(query);
-
-				return Content(lst);
-			}
-			catch (Exception ex)
-			{
-				return InternalServerError(ex);
-			}
-		}
-
-		#endregion
-
-		#region GetBlogById
-
-		[HttpGet("{id}")]
-		public async Task<IActionResult> GetBlogById(long id)
-		{
-			try
-			{
-				var query = new GetBlogByIdQuery() { BlogId = id };
-				var item = await _mediator.Send(query);
-
-				return Content(item);
-			}	
-			catch (Exception ex)
-			{
-				return InternalServerError(ex);
-			}
-		}
-
-		#endregion
-
+		_mediator = mediator;
 	}
+
+	#region GetBlogs
+
+	[HttpGet]
+	public async Task<IActionResult> GetBlogs()
+	{
+		try
+		{
+			var query = new GetBlogListQuery();
+			var lst = await _mediator.Send(query);
+
+			return Content(lst);
+		}
+		catch (Exception ex)
+		{
+			return InternalServerError(ex);
+		}
+	}
+
+	#endregion
+
+	#region GetBlogById
+
+	[HttpGet("{id}")]
+	public async Task<IActionResult> GetBlogById(long id)
+	{
+		try
+		{
+			var query = new GetBlogByIdQuery() { BlogId = id };
+			var item = await _mediator.Send(query);
+
+			return Content(item);
+		}	
+		catch (Exception ex)
+		{
+			return InternalServerError(ex);
+		}
+	}
+
+	#endregion
+
 }
