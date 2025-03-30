4d33a2b0ed574fce93f3f1e9cc4b42a707fa38bc
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-30 15:10:26 +0630

    Format Code

--- DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Command/DeleteBlog/DeleteBlogCommandHandler.cs
+++ DotNet8.VerticleSlice&CQRSExample.Api/Features/Blog/Command/DeleteBlog/DeleteBlogCommandHandler.cs
@@ -1,20 +1,19 @@
-﻿namespace DotNet8.VerticleSlice_CQRSExample.Api.Features.Blog.Command.DeleteBlog
+﻿namespace DotNet8.VerticleSlice_CQRSExample.Api.Features.Blog.Command.DeleteBlog;
+
+public class DeleteBlogCommandHandler : IRequestHandler<DeleteBlogCommand, int>
 {
-	public class DeleteBlogCommandHandler : IRequestHandler<DeleteBlogCommand, int>
+	private readonly IBlogRepository _blogRepository;
+
+	public DeleteBlogCommandHandler(IBlogRepository blogRepository)
 	{
-		private readonly IBlogRepository _blogRepository;
+		_blogRepository = blogRepository;
+	}
 
-		public DeleteBlogCommandHandler(IBlogRepository blogRepository)
-		{
-			_blogRepository = blogRepository;
-		}
+	public async Task<int> Handle(DeleteBlogCommand request, CancellationToken cancellationToken)
+	{
+		if (request.BlogId <= 0)
+			throw new Exception("Id is invalid.");
 
-		public async Task<int> Handle(DeleteBlogCommand request, CancellationToken cancellationToken)
-		{
-			if (request.BlogId <= 0)
-				throw new Exception("Id is invalid.");
-
-			return await _blogRepository.DeleteBlogAsync(request.BlogId);
-		}
+		return await _blogRepository.DeleteBlogAsync(request.BlogId);
 	}
 }
